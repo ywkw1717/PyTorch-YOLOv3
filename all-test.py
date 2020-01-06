50eb3967954bb588dccd0605ebd40acab8dae3f2
yyy <y.w.k.w.1717@gmail.com>
2020-01-07 03:09:39 +0900

    Modify conf_thres

--- all-test.py
+++ all-test.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python
 import os
 import glob
+import sys
 
 
 def main():
     f = glob.glob('./checkpoints/*')
     for s in f:
         print(s)
-        os.system('python3 -W ignore:UserWarning test.py --model_def config/malimg.cfg --data_config config/malimg.data --weights_path ' + s + ' --conf_thres 0.8')
+        os.system('python3 -W ignore:UserWarning test.py --model_def config/malimg.cfg --data_config config/malimg.data --weights_path ' + s + ' --conf_thres ' + sys.argv[1])
 
 
 if __name__ == "__main__":
