bd5c0fc1f9e9b081f90607000d61fd639ebb4772
yyy <y.w.k.w.1717@gmail.com>
2020-01-25 10:21:06 +0900

    Modify to selectable checkpoints directory in all-test.py

--- all-test.py
+++ all-test.py
@@ -4,11 +4,13 @@ import glob
 import sys
 
 
+# How to use:
+# python3 all-test.py checkpoints_path conf_thres
 def main():
-    f = glob.glob('./checkpoints/*')
+    f = glob.glob(sys.argv[1] + '/*')
     for s in f:
         print(s)
-        os.system('python3 -W ignore:UserWarning test.py --model_def config/malimg.cfg --data_config config/malimg.data --weights_path ' + s + ' --conf_thres ' + sys.argv[1])
+        os.system('python3 -W ignore:UserWarning test.py --model_def config/malimg.cfg --data_config config/malimg.data --weights_path ' + s + ' --conf_thres ' + sys.argv[2])
 
 
 if __name__ == "__main__":
