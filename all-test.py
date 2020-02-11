c94f71da0e1602e6b707f644df2609d9ff7a4cb8
yyy <y.w.k.w.1717@gmail.com>
2020-02-12 02:56:37 +0900

    Delete class path

--- all-test.py
+++ all-test.py
@@ -6,14 +6,13 @@ import re
 
 
 # How to use:
-# python3 all-test.py class_path cfg_path data_path checkpoints_path conf_thres starting_from
+# python3 all-test.py cfg_path data_path checkpoints_path conf_thres starting_from
 def main():
-    class_path = sys.argv[1]
-    cfg_path = sys.argv[2]
-    data_path = sys.argv[3]
-    checkpoints_path = sys.argv[4]
-    conf_thres = sys.argv[5]
-    starting_from = int(sys.argv[6])
+    cfg_path = sys.argv[1]
+    data_path = sys.argv[2]
+    checkpoints_path = sys.argv[3]
+    conf_thres = sys.argv[4]
+    starting_from = int(sys.argv[5])
 
     f = glob.glob(checkpoints_path + '/*')
     f = sorted(f, key=lambda x: int(re.findall(r"\d+", os.path.basename(x))[1]))
@@ -23,7 +22,7 @@ def main():
 
     for s in f:
         print(s)
-        os.system('python3 -W ignore:UserWarning test.py --class_path ' + class_path + ' --model_def ' + cfg_path + ' --data_config ' + data_path + ' --weights_path ' + s + ' --conf_thres ' + conf_thres)
+        os.system('python3 -W ignore:UserWarning test.py --model_def ' + cfg_path + ' --data_config ' + data_path + ' --weights_path ' + s + ' --conf_thres ' + conf_thres)
 
 
 if __name__ == "__main__":
